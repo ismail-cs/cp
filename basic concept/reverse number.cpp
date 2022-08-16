635b5bdda2b2ab466a46d4d54fca891037a1fa13
isamil-cs <ismail.k.aiub@gmail.com>
2022-08-17 01:09:34 +0600

    reverse number code added

---
+++ basic concept/reverse number.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+int main(){
+	
+	int num,sum=0,r,temp;
+	
+	printf("enter a numbe : ");
+	scanf("%d", &num);
+	
+	temp = num;
+	
+	while(temp != 0){
+		r = temp % 10;
+		sum = sum * 10 + r;
+		temp = temp / 10;
+	}
+	
+	printf("reverse number is : %d\n", sum);
+}
