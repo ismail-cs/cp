3eb9ca49939921f50e8b1dc8a7e6d1d8f1f231eb
isamil-cs <ismail.k.aiub@gmail.com>
2022-08-26 18:19:17 +0600

    ternary operator cpp file added

---
+++ basic concept/ternary_operator.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int num;
+	cin>>num;
+	
+	// ternary operator
+	
+	// cout<<( ? : );
+	
+	// cout<<( condition ? if true : if false)<<endl;
+	
+	cout<<( num % 2 == 0 ? "even_number" : "odd_number")<<endl;
+	
+}
