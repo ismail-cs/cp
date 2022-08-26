a574430dda252f947e27cb864ab5d40179089b3e
isamil-cs <ismail.k.aiub@gmail.com>
2022-08-26 17:31:21 +0600

    type_casting cpp file added

---
+++ basic concept/type_casting.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	
+	int a=5, b=2;
+	double reasult;
+	
+	reasult = (double)a / b;  // C style  type_casting
+	
+	reasult = double(a) / b;  // C++ style  type_casting
+	
+	cout<<reasult<<endl;
+	
+}
