5618f8b241e83e72a72f0e52a4012dd03f17cba3
ismail khalil <62925102+ismail-cs@users.noreply.github.com>
2022-12-21 03:19:52 +0600

    Update binary search.cpp

--- algorithm/binary search.cpp
+++ algorithm/binary search.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-			  //index  0  1  2  3  4  5   6   7   8   9   10
+	          //index  0  1  2  3  4  5   6   7   8   9   10
 	vector<int> vec = {1, 2, 4, 5, 8, 10, 12, 23, 25, 36, 67};
 					  
 	
