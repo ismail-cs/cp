1101ccbb011f513d71f81a6088e4fa12aa3bdd31
ismail khalil <62925102+ismail-cs@users.noreply.github.com>
2023-03-22 02:34:51 +0600

    Update binary search.cpp
    first update

--- algorithm/binary search.cpp
+++ algorithm/binary search.cpp
@@ -10,32 +10,30 @@ int main()
 	sort( vec.begin(), vec.end() );
 	
 	int target; cin>>target;
+	int ans = -1;
+	int l = 0, r = vec.size()-1; 
 	
-	int start = 0, end = vec.size()-1; 
-	
-	while( start <= end ){
+	while( l <= r ){
 		
-		int mid = ( start + end ) / 2;
+		int mid = ( l + r ) / 2;
 		
-		if ( target < vec[mid] ){
-			end = mid - 1;
-		} 
-		else if ( target >  vec[mid] ){
-			start = mid + 1;
-		}
-		else{
-			end = start = mid;
+		if( vec[mid] == target ){
+			ans = mid;
 			break;
 		}
+		if ( target < vec[mid] ){
+			r = mid - 1;
+		} 
+		else if ( target >  vec[mid] ){
+			l = mid + 1;
+		}
 	}
 	
-	if ( end == start ){
-		cout<<"yes \n"<<"index "<<end<<endl;
+	if ( ans != -1 ){
+		cout <<"yes. index : "<< ans << endl;
 	}
 	else{
-		cout<<"no"<<endl;
+		cout <<"no" <<endl;
 	}
-
 }
-	
 
