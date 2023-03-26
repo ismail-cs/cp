aafffa2c44a1dd0fe4765adcec4c908f94a49546
ismail khalil <62925102+ismail-cs@users.noreply.github.com>
2023-03-27 01:20:20 +0600

    Create underected_multi_edge.cpp
    add

---
+++ algorithm/Graph/underected_multi_edge.cpp
@@ -0,0 +1,65 @@
+
+
+// underected graph and multi edge input by adjacency list
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int mx = 1e5+123;
+vector < pair < int, int> > adj[mx];
+
+int main(){
+	
+	int node, edge;   cin >> node >> edge;
+	
+	while(edge--){
+		int n1, n2, w;   cin >> n1 >> n2 >> w;
+		adj[n1].push_back({n2, w});
+		adj[n2].push_back({n1, w});
+	}
+	
+	for(int i=1; i<=node; i++){
+		cout << i <<" : ";
+		for(auto u:adj[i]){
+			cout <<"("<< u.first <<" "<<u.second<<")"<<" ";
+		}
+		cout <<endl;
+	}
+}
+
+
+/*
+ 
+                      3
+               [1]----------[2].
+              /	            .|\   .
+             /            .  | \     . 3
+            /           .    |  \     .
+           /           .     |   \1    .
+          /           .      |    \    .
+       4 /        	 .       |     \   .
+        /           .        |      \ .
+       /      	  7 .       5|       [4]
+      /             .        |      
+     /    	         .       |     
+    /     	       	  .      |    
+   /      	           .     |   
+  /                      .   |  
+ [5]     		              .  | 
+      		                  .|
+      		                  [3]
+      		
+    
+    
+  edge:
+    		
+  1  2  3
+  1  5  4
+  2  3  5
+  2  3  7
+  2  4  1
+  2  4  3
+    
+*/
+    
