93b554cf8848c0116544fb0d373ec86818f45fcd
ismail khalil <62925102+ismail-cs@users.noreply.github.com>
2023-04-06 04:21:40 +0600

    Update Histogram.cpp
    update formate

--- solution/Lightoj/Histogram.cpp
+++ solution/Lightoj/Histogram.cpp
@@ -1,36 +1,41 @@
 
 // Problem solution "Histogram" lightoj 
 
-
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
+	
 	int tc;  cin >> tc;
 	for(int j=1; j<=tc; j++){
+		
 		int n; cin >> n;
-		vector<pair<int, int>> vp;
-		vector<int> arr1, arr2;
+		vector < pair < int, int > > vp;
+		vector < int > arr1, arr2;
 		
 		for(int i=0; i<n; i++) {
+			
 			int a; cin >> a;
-			vp.push_back({a, i});
+			vp.push_back ( {a, i} );
 		}
 		
-		stack < pair < int, int>> st;   
-		st.push({-1, -1});
+		stack < pair < int, int > > st;   
+		st.push ( {-1, -1} );
 		
-		for(int i=0; i<n; i++){
-			if(st.top().first < vp[i].first){
-				arr1.push_back(st.top().second);
-				st.push({vp[i].first, vp[i].second});
+		for( int i=0; i<n; i++ ) {
+			
+			if( st.top().first < vp[i].first ) {
+				
+				arr1.push_back( st.top().second );
+				st.push( {vp[i].first, vp[i].second} );
 			}
 			else{
-				while(st.top().first >= vp[i].first){
+				
+				while( st.top().first >= vp[i].first ) {
 					st.pop();
 				}
 				arr1.push_back(st.top().second);
-				st.push({vp[i].first, vp[i].second});
+				st.push( {vp[i].first, vp[i].second} );
 			}
 		}
 		
@@ -38,32 +43,47 @@ int main(){
 			st.pop();
 		}
 		
-		st.push({-1, -1});
-		for(int i=n-1; i>=0; i--){
-			if(st.top().first < vp[i].first){
-				if(st.top().second == -1) arr2.push_back(n);
-				else arr2.push_back(st.top().second);
-				st.push({vp[i].first, vp[i].second});
+		st.push ( {-1, -1} );
+		
+		for(int i=n-1; i>=0; i--) {
+			
+			if( st.top().first < vp[i].first ) {
+				
+				if( st.top().second == -1 ) {
+					arr2.push_back(n);
+				}
+				else {
+					arr2.push_back(st.top().second);
+				}
+				st.push( {vp[i].first, vp[i].second} );
 			}
+			
 			else{
-				while(st.top().first >= vp[i].first){
+				
+				while( st.top().first >= vp[i].first ) {
 					st.pop();
 				}
-				if(st.top().second == -1) arr2.push_back(n);
-				else arr2.push_back(st.top().second);
-				st.push({vp[i].first, vp[i].second});
+				
+				if( st.top().second == -1 ) {
+					arr2.push_back(n);
+				}
+				else {
+					arr2.push_back(st.top().second);
+				}
+				st.push( {vp[i].first, vp[i].second} );
 			}
 		}
-		reverse(arr2.begin(), arr2.end());
+		
+		reverse( arr2.begin(), arr2.end() );
 		
 		int result = -1;
 		
 		for(int i=0; i<n; i++){
-			int str = (arr2[i]-arr1[i]-1)*vp[i].first;
-			result = max(result, str);
+			int str = (arr2[i]-arr1[i]-1) * vp[i].first;
+			result = max( result, str );
 		}
 		
-		cout <<"Case "<<j<<": "<< result <<endl;
+		cout << "Case " << j << ": " << result <<endl;
 	}
 	
 }
