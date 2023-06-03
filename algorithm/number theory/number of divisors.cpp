04cad280566123b7552776b8e9f65b812b10078d
ismail khalil <62925102+ismail-cs@users.noreply.github.com>
2023-06-03 23:16:51 +0600

    Create number of divisors.cpp

---
+++ algorithm/number theory/number of divisors.cpp
@@ -0,0 +1,28 @@
+
+// find number of divisors of all numbers 1 to N
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int mx = 1e7+123;
+int cnt[mx];
+
+int main(){
+	
+	
+	int lim=100;
+	
+	for(int i=1; i<=lim; i++){
+		
+		for(int j=i; j<=lim; j+=i){
+			cnt[j]++;
+		}
+	}
+	
+	
+	for(int i=1; i<lim; i++){
+		cout << i << ":" << cnt[i] <<endl;
+	}
+	
+}
