19518281a0ef1cd4af20f32bc336478e7d53617d
ismail khalil <62925102+ismail-cs@users.noreply.github.com>
2023-06-05 02:34:49 +0600

    Create Optimized Sieve.cpp

---
+++ algorithm/Number theory/Optimized Sieve.cpp
@@ -0,0 +1,44 @@
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int mx=1e8;
+bitset<mx> isPrime;
+vector<int> primes;
+
+void primeGen( int n ){
+	
+	for(int i=3; i<=n; i+=2) isPrime[i] = 1;
+	
+	int sq = sqrt(n);
+	for(int i = 3; i <= sq; i+=2){
+		if (isPrime[i]){
+			for( int j=i*i; j<=n; j+=i){
+				isPrime[j] = 0;
+			}
+		}
+	}
+	
+	primes.push_back(2);
+	for(int i=3; i<=n; i+=2){
+		if(isPrime[i] == 1){
+			primes.push_back(i);
+		}
+	}
+}
+
+int main(){
+	
+	int lim = 1e8;
+	primeGen( lim );
+	
+	int size = primes.size();
+	for(int i=0; i<size; i++){
+		cout << primes[i] <<endl;
+	}
+	cout << size <<endl;
+
+}
+
