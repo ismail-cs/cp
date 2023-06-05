44cc5803b85367271f6c6bc255f572d79965e0d9
ismail khalil <62925102+ismail-cs@users.noreply.github.com>
2023-06-05 17:44:29 +0600

    Create all divisors of a number.cpp

---
+++ algorithm/Number theory/all divisors of a number.cpp
@@ -0,0 +1,31 @@
+
+// find out all divisors of a number
+// useing harmony series
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int mx = 1e7+123;
+vector<int> dv[mx];
+
+int main(){
+	
+	int lim = 100;
+	
+	for(int i = 1; i <= lim; i++){
+		for(int j = i; j <= lim; j += i){
+			dv[j].push_back(i);
+		}
+	}
+	
+	
+	for(int i=1; i<=lim; i++){
+		
+		cout << i <<" : ";
+		for(auto u:dv[i]){
+			cout << u <<" ";
+		}
+		cout <<endl;
+	}
+	
+}
