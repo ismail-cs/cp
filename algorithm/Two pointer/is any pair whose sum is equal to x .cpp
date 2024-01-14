a52baa67fd0fc9ae021751536e04c76d4fb7f542
ismail khalil <ismail.k.aiub@gmail.com>
2024-01-15 00:50:07 +0600

    Create is any pair whose sum is equal to x .cpp

---
+++ algorithm/Two pointer/is any pair whose sum is equal to x .cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool solve(int n, int x, vector<int> v){
+	
+	int i=0, j=n-1;
+	
+	int sum = 0;
+	while(i < j){
+    
+		sum = v[i] + v[j];
+		if(sum == x) return true;
+		else if(sum < x) i++;
+		else j--; 
+    
+	} 
+  
+	return false;
+}
+
+
+int main() {
+	
+	int n, x; 
+	cin >> n >> x;
+	
+	vector<int> v(n);
+	
+	for(auto &u : v){
+		cin >> u;
+	}
+	
+	if(solve(n, x, v)){
+		cout << "yes exists"<<endl;
+	}
+  else{
+		cout <<"not exists" <<endl;
+	}
+  
+}
+
+
+/*
+
+input:   
+5 9
+1 2 3 4 5
+  
+output:
+yes exists
+
+
+
+input:   
+5 10
+1 2 3 4 5
+  
+output:
+not exists
+
+*/
